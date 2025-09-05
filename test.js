8de841305d0317f4a0f2669b1cb3a7ff1aca87c6
Kushagra <kushagrashukla707@gmail.com>
2025-09-06 01:01:20 +0530

    first commit

--- test.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-const rawData = fs.readFileSync('data.json', 'utf8');
-const jsonData = JSON.parse(rawData);
-console.log("jsonData : ", jsonData)
-console.log("jsonData['icp'] : ", jsonData["icp"])
